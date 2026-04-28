d47845d5bd31a5a879b16784bd0e75b09f9ea69a
leoletsplayer <leonardzerr@gmail.com>
2026-04-28 20:03:17 +0200

    so

--- TempMesser/src/main.cpp
+++ TempMesser/src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "LiquidCrystal_PCF8574.h"
+LiquidCrystal_PCF8574 display(0x27);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define Adresse 0x4F
+unsigned char temp_register = 0;
+unsigned char temp_lsb;
+signed char temp_msb;
+
+void setup()
+{
+  display.begin(16,2);
+  display.setBacklight(255);
+  display.clear();
+  display.setCursor(0,0);
+  display.printf("it.schule");
+
+  Wire.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  Wire.beginTransmission(Adresse);
+  Wire.write(temp_register);
+  Wire.endTransmission(false);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Wire.requestFrom(Adresse, 2);
+  temp_msb = Wire.read();
+  temp_lsb = Wire.read();
+  Wire.endTransmission();
+
+  display.setCursor(0,1);
+  display.printf("Temp: %d C*",temp_msb);
 }
